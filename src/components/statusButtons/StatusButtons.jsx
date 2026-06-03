4b85a027a5923e060897d95508a0b37bbd53588a
James <whitej@allsaintsmat.org>
2026-06-03 23:04:45 +0100

    Update status buttons

--- src/components/statusButtons/StatusButtons.jsx
+++ src/components/statusButtons/StatusButtons.jsx
@@ -17,7 +17,7 @@ const computer_statuses = [
 ];
 
 const hasSimpleStatuses = (itemType) =>
-  itemType === "Staff laptop" || itemType === "Staff computer" || itemType === "Radio" || itemType === "Mobile phone" || itemType === "IP phone" || itemType === "Staff Ipad";
+  itemType === "Staff laptop" || itemType === "Staff computer" || itemType === "Radio" || itemType === "Mobile phone" || itemType === "IP phone" || itemType === "Staff ipad";
 
 const mapStatusForDisplay = (status, itemType) => {
   if (!hasSimpleStatuses(itemType)) return status;
